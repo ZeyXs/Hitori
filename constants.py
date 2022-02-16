0aa44e764f0b9b41c635186a5c6f91e830d42f8a
ZeyX <basile.gamer@gmail.com>
2022-02-16 15:54:05 +0100

    Added resourcePath()

--- constants.py
+++ constants.py
@@ -6,20 +6,30 @@ pygame.font.init()
 if getattr(sys, 'frozen', False):
     wd = sys._MEIPASS
 else:
-    wd = ''   
+    wd = ''
+
+def resourcePath(relativePath):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        basePath = sys._MEIPASS
+    except Exception:
+        basePath = os.path.abspath(".")
+
+    return os.path.join(basePath, relativePath)
 
 # --- TEXTURES ---
-LOGO = pygame.image.load(os.path.join(wd,'resources',"favicon.png"))
-GUI_TIMER = pygame.image.load(os.path.join(wd,'resources',"gui_timer.png"))
+LOGO = pygame.image.load(resourcePath("resources/favicon.png"))
+GUI_TIMER = pygame.image.load(resourcePath("resources/gui_timer.png"))
 
-PAUSE = pygame.image.load(os.path.join(wd,'resources',"pause_button.png"))
-PAUSE_PRESSED = pygame.image.load(os.path.join(wd,'resources',"pause_button_pressed.png"))
+PAUSE = pygame.image.load(resourcePath("resources/pause_button.png"))
+PAUSE_PRESSED = pygame.image.load(resourcePath("resources/pause_button_pressed.png"))
 
-RESTART = pygame.image.load(os.path.join(wd,'resources',"restart_button.png"))
-RESTART_PRESSED = pygame.image.load(os.path.join(wd,'resources',"restart_button_pressed.png"))
+RESTART = pygame.image.load(resourcePath("resources/restart_button.png"))
+RESTART_PRESSED = pygame.image.load(resourcePath("resources/restart_button_pressed.png"))
 
-CHECK = pygame.image.load(os.path.join(wd,'resources',"check_button.png"))
-CHECK_PRESSED = pygame.image.load(os.path.join(wd,'resources',"check_button_pressed.png"))
+CHECK = pygame.image.load(resourcePath("resources/check_button.png"))
+CHECK_PRESSED = pygame.image.load(resourcePath("resources/check_button_pressed.png"))
 
 # --- COLORS ---
 BACKGROUND_COLOR = (247, 247, 247)
