622de0076932e4b94d55d6c08404353e413b657b
ZeyX <basile.gamer@gmail.com>
2022-02-16 13:30:26 +0100

    Fixed constants.py for .exe

--- constants.py
+++ constants.py
@@ -1,18 +1,25 @@
 import pygame
+import os
+import sys
 pygame.font.init()
 
+if getattr(sys, 'frozen', False):
+    wd = sys._MEIPASS
+else:
+    wd = ''   
+
 # --- TEXTURES ---
-LOGO = pygame.image.load("resources/favicon.png")
-GUI_TIMER = pygame.image.load("resources/gui_timer.png")
+LOGO = pygame.image.load(os.path.join(wd,'resources',"favicon.png"))
+GUI_TIMER = pygame.image.load(os.path.join(wd,'resources',"gui_timer.png"))
 
-PAUSE = pygame.image.load("resources/pause_button.png")
-PAUSE_PRESSED = pygame.image.load("resources/pause_button_pressed.png")
+PAUSE = pygame.image.load(os.path.join(wd,'resources',"pause_button.png"))
+PAUSE_PRESSED = pygame.image.load(os.path.join(wd,'resources',"pause_button_pressed.png"))
 
-RESTART = pygame.image.load("resources/restart_button.png")
-RESTART_PRESSED = pygame.image.load("resources/restart_button_pressed.png")
+RESTART = pygame.image.load(os.path.join(wd,'resources',"restart_button.png"))
+RESTART_PRESSED = pygame.image.load(os.path.join(wd,'resources',"restart_button_pressed.png"))
 
-CHECK = pygame.image.load("resources/check_button.png")
-CHECK_PRESSED = pygame.image.load("resources/check_button_pressed.png")
+CHECK = pygame.image.load(os.path.join(wd,'resources',"check_button.png"))
+CHECK_PRESSED = pygame.image.load(os.path.join(wd,'resources',"check_button_pressed.png"))
 
 # --- COLORS ---
 BACKGROUND_COLOR = (247, 247, 247)
